2a4087ef7760ad8a8a8509b91227b611d24d1b01
Roberto Martinez <122717623+RMartinezCaballero@users.noreply.github.com>
2024-12-23 23:53:10 -0500

    Challenge Conversor de Moneda Terminado
    Challenge Conversor de Moneda Terminado
    
    DEV Back-End @RMartinezCaballero G7 ONE

---
+++ Conversor-de-Monedas-main/Conversor-de-Monedas-main/src/Principal.java
@@ -0,0 +1,88 @@
+import com.google.gson.JsonSyntaxException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int opcionElegida = 0;
+        ConsultaConversion consulta = new ConsultaConversion();
+        Calculos calculos = new Calculos(consulta);
+        GeneradorDeArchivos generador = new GeneradorDeArchivos();
+        List<String> respuestas = new ArrayList<>();
+        String menu = """
+                \n***************************************************
+                *** Hola Bienvenido al Conversor de Monedas ***
+                
+                1) Dólar ==>> Peso Argentino
+                2) Peso Argentino ==>> Dólar
+                3) Dólar  ==>> Real Brasileño
+                4) Real Brasileño ==>> Dólar
+                5) Dólar ==>> Peso Colombiano
+                6) Peso Colombiano ==>> Dólar
+                
+                7) Otras opciónes de conversión
+                
+                8) Salir
+                ***************************************************
+                """;
+
+        while (opcionElegida != 8) {
+            try {
+                System.out.println(menu);
+                opcionElegida = Integer.parseInt(lectura.nextLine());
+                LocalDateTime myDateObj = LocalDateTime.now(); // Obtener la marca de tiempo actual
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = myDateObj.format(myFormatObj);
+
+                switch (opcionElegida) {
+                    case 1:
+                        calculos.almacenarValores("USD", "ARS");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 2:
+                        calculos.almacenarValores("ARS", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 3:
+                        calculos.almacenarValores("USD", "BRL");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 4:
+                        calculos.almacenarValores("BRL", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 5:
+                        calculos.almacenarValores("USD", "COP");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 6:
+                        calculos.almacenarValores("COP", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 7:
+                        calculos.almacenarValoresPersonalizados();
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        System.out.println("Ingrese una opción válida");
+                }
+            } catch (JsonSyntaxException | NullPointerException e) {
+                System.out.println("Error. Ingrese solo códigos de moneda válidos.");
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Error. Ingrese un valor numérico válido.");
+            }
+        }
+        generador.guardarJson(respuestas);
+
+        System.out.println("Saliendo del Conversor" +
+                "Desarrollado Por @RMartinezC - DEV G7 Oracle One Backend");
+    }
+}
